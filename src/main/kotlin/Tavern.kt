16bebe606dc4b456cd58f1906a5099e2d97433d3
Евгений Афанасьев <evgenij29082002@gmail.com>
2023-05-08 14:27:13 +0800

    kotlin string

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,19 +1,33 @@
-import java.util.*
-
+const val TAVERN_NAME = "Taernyl's Folly"
 fun main() {
-//     var beverage = readlnOrNull()?.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-//    var beverage = readLine()?.let {
-//        if (it.isNotBlank()) it.capitalize()
-//        else "Buttered Ale"
-//    }
-//    beverage = null
-//    var beverage = readlnOrNull()!!.capitalize() // ТОЛЬКО КОГДА УВЕРЕН, ЧТО NULL НЕ БУДЕТ, ИНАЧЕ ВЫБРОСИТСЯ NULLPOINTEREXCEPTION
-    var beverage = readLine()
-    beverage?.let {
-        beverage = it.capitalize()
-    } ?: println("I can't do that without crashing - beverage was null!")
-
-    var beverageServed: String = beverage ?: "Buttered Ale"
-
-     println(beverage)
+//    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
  }
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Medrigal buys a $name ($type) for $price."
+    println(message)
+
+    val phrase = if (name == "Dragon's Breath")
+        "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
+    else
+        "Madrigal says: Thanks for the $name."
+    println(phrase)
+    println()
+    println(toDragonSpeak("DRAGON'S BREATH: IT'S GOT WHAT ADVENTURES CRAVE"))
+}
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when(it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "0" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
